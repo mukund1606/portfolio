03e622468f1a4a630cbd2c860e48db7658e42766
Mukund Mittal <mukund.mukulmittal@gmail.com>
2023-07-26 07:27:05 +0530

    Mobile Nav Added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mukund.page",
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), react()]
 });
