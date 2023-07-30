3714bc5c45c1c1b3c4d3acb9e8dc056c0931585e
Mukund Mittal <mukund.mukulmittal@gmail.com>
2023-07-30 19:33:09 +0530

    Sitemap Updated

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,5 +6,29 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: "https://mukund.page/",
       lastModified: new Date(),
     },
+    {
+      url: "https://mukund.page/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://mukund.page/tech-stack",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://mukund.page/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://mukund.page/projects",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://mukund.page/contact",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://mukund.page/blog",
+      lastModified: new Date(),
+    },
   ];
 }
